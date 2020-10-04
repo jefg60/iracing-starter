a28decad73f7ebb68271bf03a2b4282f1e9c53d3
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-04 09:33:29 +0100

    add crewchief, put iracing first

--- solorace.ps1
+++ solorace.ps1
@@ -1,7 +1,11 @@
-﻿# JRT
+﻿# iRacing
+$iRacingdir = "C:\Program Files (x86)\iRacing\ui\"
+Start-Process -WorkingDirectory $iRacingdir -FilePath "$iRacingdir\iRacingUI.exe"
+
+# JRT
 $JRTdir = 'E:\Joel Real Timing - no CUDA\'
 Start-Process -WorkingDirectory $JRTdir -FilePath "$JRTdir\Timing.exe"
 
-# iRacing
-$iRacingdir = "C:\Program Files (x86)\iRacing\ui\"
-Start-Process -WorkingDirectory $iRacingdir -FilePath "$iRacingdir\iRacingUI.exe"
+# CrewChief
+$CCdir = "C:\Program Files (x86)\Britton IT Ltd\CrewChiefV4\"
+Start-Process -WorkingDirectory $CCdir -FilePath "$CCdir\CrewChiefV4.exe"
