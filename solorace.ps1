5a61cd57738df6164590ce156407b7b1c1962607
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-04 09:30:53 +0100

    iracing and jrt

---
+++ solorace.ps1
@@ -0,0 +1,7 @@
+﻿# JRT
+$JRTdir = 'E:\Joel Real Timing - no CUDA\'
+Start-Process -WorkingDirectory $JRTdir -FilePath "$JRTdir\Timing.exe"
+
+# iRacing
+$iRacingdir = "C:\Program Files (x86)\iRacing\ui\"
+Start-Process -WorkingDirectory $iRacingdir -FilePath "$iRacingdir\iRacingUI.exe"
