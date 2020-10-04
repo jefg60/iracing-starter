a01c59920d12dd0a19d27187177f78fadc9b3bc4
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-04 10:39:01 +0100

    add OBS

--- solorace.ps1
+++ solorace.ps1
@@ -1,10 +1,11 @@
-﻿# $App = directory,executable
+﻿# $App = directory,executable,
 $iRacing = "C:\Program Files (x86)\iRacing\ui","iRacingUI.exe"
 $JoelRealTiming = "E:\Joel Real Timing - no CUDA","Timing.exe"
 $CrewChief = "C:\Program Files (x86)\Britton IT Ltd\CrewChiefV4","CrewChiefV4.exe"
 $TradingPaints = "C:\Program Files (x86)\Rhinode LLC\Trading Paints","Trading Paints.exe"
+$OBS_Studio = "C:\Program Files\obs-studio\bin\64bit","obs64.exe"
 
-$solorace = $iRacing,$JoelRealTiming,$CrewChief,$TradingPaints
+$solorace = $iRacing,$JoelRealTiming,$OBS_Studio,$CrewChief,$TradingPaints
 
 function start-app {
     param (
