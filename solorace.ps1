0837eef6c64bcf73e15c7713d7e6ba7076da3306
Jeff Hibberd <jeff@jeffhibberd.com>
2020-10-04 10:22:14 +0100

    use functions and arrays

--- solorace.ps1
+++ solorace.ps1
@@ -1,11 +1,26 @@
-﻿# iRacing
-$iRacingdir = "C:\Program Files (x86)\iRacing\ui\"
-Start-Process -WorkingDirectory $iRacingdir -FilePath "$iRacingdir\iRacingUI.exe"
+﻿# $App = directory,executable
+$iRacing = "C:\Program Files (x86)\iRacing\ui","iRacingUI.exe"
+$JoelRealTiming = "E:\Joel Real Timing - no CUDA","Timing.exe"
+$CrewChief = "C:\Program Files (x86)\Britton IT Ltd\CrewChiefV4","CrewChiefV4.exe"
+$TradingPaints = "C:\Program Files (x86)\Rhinode LLC\Trading Paints","Trading Paints.exe"
 
-# JRT
-$JRTdir = 'E:\Joel Real Timing - no CUDA\'
-Start-Process -WorkingDirectory $JRTdir -FilePath "$JRTdir\Timing.exe"
+$solorace = $iRacing,$JoelRealTiming,$CrewChief,$TradingPaints
 
-# CrewChief
-$CCdir = "C:\Program Files (x86)\Britton IT Ltd\CrewChiefV4\"
-Start-Process -WorkingDirectory $CCdir -FilePath "$CCdir\CrewChiefV4.exe"
+function start-app {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$workingdir,
+        [Parameter(Mandatory=$true)]
+        [string]$executable
+    )
+    Start-Process -WorkingDirectory $workingdir -FilePath "$workingdir\$executable"
+}
+
+ForEach ($app in $solorace) {
+    Write-Host "Checking for $app"
+    $alreadyrunning = Get-Process $app[1].Split(".")[0] -ErrorAction SilentlyContinue
+    if ( -Not $alreadyrunning ) {
+        Write-Host "Starting $app"
+        start-app -workingdir $app[0] -executable $app[1]
+    }
+}
